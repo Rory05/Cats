686190642c64eb7edbb2db45dbff58d10a66520f
Masha Lisova <Romashka.124578369@gmail.com>
2020-09-06 20:12:49 +0300

    Добавьте файлы проекта.

---
+++ Web_Lab1_Cats/Cats.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Web_Lab1_Cats
+{
+    public partial class Cats
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public byte[] Photo { get; set; }
+        public int SpeciesId { get; set; }
+        public int Age { get; set; }
+        public string Info { get; set; }
+    }
+}
---
+++ Web_Lab1_Cats/Species.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Web_Lab1_Cats
+{
+    public partial class Species
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public string Wool { get; set; }
+        public string Size { get; set; }
+        public int Lifetime { get; set; }
+    }
+}
