382126932aa98158724cee053e7a6772e9a971cd
Masha Lisova <Romashka.124578369@gmail.com>
2020-09-07 23:52:30 +0300

    Текст

--- Web_Lab1_Cats/wwwroot/js/site.js
+++ Web_Lab1_Cats/wwwroot/js/site.js
@@ -2,3 +2,6 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    $("body").css("opacity", "1");
+});
