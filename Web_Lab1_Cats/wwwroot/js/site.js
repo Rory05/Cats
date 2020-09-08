2c509205ddfd37dbbd0bd38d0e166dd8f65b28aa
SofiaBuleiko <sofiabulejko@gmail.com>
2020-09-08 11:06:36 +0300

    popytka2

--- Web_Lab1_Cats/wwwroot/js/site.js
+++ Web_Lab1_Cats/wwwroot/js/site.js
@@ -4,4 +4,21 @@
 // Write your JavaScript code.
 $(document).ready(function () {
     $("body").css("opacity", "1");
-});
+}); //Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.documentElement.style.transition = 7;
+    document.documentElement.scrollTop = 0;
+}
